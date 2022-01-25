a958610fe14ac7da4074ce947c91a81130a5509d
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-26 03:01:28 +0900

    lambda handler

---
+++ aws-lambda/app.py
@@ -0,0 +1,15 @@
+import pymysql
+
+from kw_home import crawl_kw_home
+from sw_central import crawl_sw_central
+from db_connection import connect_to_rds
+
+def handler(event, context):
+    # DB connection
+    conn, cursor = connect_to_rds()
+
+    # Crawling
+    crawl_kw_home(conn, cursor)
+    crawl_sw_central(conn, cursor)
+
+    conn.close
