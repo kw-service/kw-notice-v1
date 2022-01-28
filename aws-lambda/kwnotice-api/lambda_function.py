a707ffbfad83b7b77f960965c43cfec6a9e71131
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2022-01-28 20:21:27 +0900

    apply sorting on return object

--- aws-lambda/kwnotice-api/lambda_function.py
+++ aws-lambda/kwnotice-api/lambda_function.py
@@ -2,14 +2,13 @@ import json
 import pymysql
 from db_connection import connect_to_rds
 
-# Return on request with invalid type
 def invalid_type_request(type):
     return {
         'status_code' : 404,
         'message' : 'Invalid type request : ' + type
     }
 
-# Return on request with invalid id
+
 def invalid_id_request(table, id):
     return {
         'status_code' : 404,
@@ -18,26 +17,22 @@ def invalid_id_request(table, id):
 
     
 def lambda_handler(event, context):
-    # Check type with request resouce path    
+        
     valid_resource_path = ['kw-home', 'sw-central']
     type = event['context']['resource-path'].split('/')[1]
     
-    # Invalid type
     if type not in valid_resource_path:
         return invalid_type_request(type)
     
-    # Get ID on path parameter
     path = event['params']['path']
     id =  None if len(path) == 0 else int(path['id'])
     
     conn, cursor = connect_to_rds()
     
-    # Set table name
     table = ''
     if type == 'kw-home': table = 'KW_HOME'
     else: table = 'SW_CENTRAL'
     
-    # MySQL query
     query = 'SELECT * from ' + table
     if id is not None:
         query += ' WHERE id = ' + str(id)
@@ -45,12 +40,10 @@ def lambda_handler(event, context):
     cursor.execute(query)
     result = cursor.fetchall()
     
-    # Empty result of notices
     if len(result) == 0: return invalid_id_request(table, id)
     
     notice_list = []
     
-    # JSON serialization
     if table == 'KW_HOME':
         for row in result:
             notice_list.append({
@@ -65,6 +58,8 @@ def lambda_handler(event, context):
                 'crawled_time' : str(row[8])
             })
             
+        notice_list.sort(key = lambda x: (x['modified_date'], x['crawled_time']), reverse=True)
+        
     elif table == 'SW_CENTRAL':
         for row in result:
             notice_list.append({
@@ -76,9 +71,6 @@ def lambda_handler(event, context):
                 'crawled_time' : str(row[5])
             })
         
-    result ={
-        'status_code' : 200,
-        'data' : notice_list
-    }
+        notice_list.sort(key = lambda x: (x['posted_date'], x['crawled_time']), reverse=True)
     
-    return result
+    return notice_list
